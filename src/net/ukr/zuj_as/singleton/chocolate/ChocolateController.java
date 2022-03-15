6d324d75d16bade774062b222ab1456038b748e3
AlekZuj <oleksandrzuiev88@gmail.com>
2022-03-15 12:21:08 +0200

    First commit

--- src/net/ukr/zuj_as/singleton/chocolate/ChocolateController.java
+++ src/net/ukr/zuj_as/singleton/chocolate/ChocolateController.java
@@ -1,13 +1,11 @@
 package net.ukr.zuj_as.singleton.chocolate;
  
 public class ChocolateController {
-	public static void main(String args[]) {
+	public static void main(String... args) {
 		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
 		boiler.fill();
 		boiler.boil();
 		boiler.drain();
-
-		// will return the existing instance
 		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
 	}
 }
